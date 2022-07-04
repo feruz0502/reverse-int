24b6fae909dd2b540ccc33e6f75666633563ae0b
feruz0502 <feruz.fgu@gmail.com>
2022-07-04 11:02:21 +0500

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+        let r=Math.abs(n);
+        let a = String(r);
+        let b = a.split('').reverse().join('');
+        let s= Number(b);
+        return s;
 }
